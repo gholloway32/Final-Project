e01f1072de60019f853ccd98fb991f60f7ea7ade
gholloway32 <97569235+gholloway32@users.noreply.github.com>
2022-04-19 18:48:16 -0400

    Final project

--- Enemys/Enemy.gd
+++ Enemys/Enemy.gd
@@ -11,9 +11,9 @@ func _physics_process(_delta):
 		Player = get_node_or_null("/root/Game/Player")
 		if Player != null:
 			look_at(Player.global_transform.origin, Vector3.UP)
-	if not $Tween.is_active():
-		$Tween.interpolate_property(self, "translation", translation, Vector3(randf()*2, randf()*1, randf()*2), 3.0, Tween.TRANS_CIRC, Tween.EASE_IN_OUT, 1.0)
-		$Tween.start()
+	#if not $Tween.is_active():
+	#	$Tween.interpolate_property(self, "translation", translation, Vector3(randf()*2, randf()*1, randf()*2), 3.0, Tween.TRANS_CIRC, Tween.EASE_IN_OUT, 1.0)
+	#	$Tween.start()
 
 func _on_Area_body_entered(body):
 	if body.name == "Player":
